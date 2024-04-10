cbc1b1dc66159ff24b1c2023f37e47aea19945d5
Kha D Pham <phamkha9x@gmail.com>
2024-04-10 10:57:21 +0700

    setup.py added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical Chatbot",
+    version="0.0.1",
+    author="Kha D Pham",
+    author_email="khapham117@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
